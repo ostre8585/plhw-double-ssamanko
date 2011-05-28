2258efb3be277dc7b081a59e747e779581f84c40
ostre8585 <ostre8585@f4b0abea-410c-0df8-8af4-832eedf32b06>
2011-05-28 09:13:08 +0000

    결과 커밋 UI대강

---
+++ trunk/pl/src/com/poolc/pl/ManageSensor.java
@@ -0,0 +1,17 @@
+package com.poolc.pl;
+
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class ManageSensor extends Activity{
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.manage_sensor);
+	}
+	
+	
+}
