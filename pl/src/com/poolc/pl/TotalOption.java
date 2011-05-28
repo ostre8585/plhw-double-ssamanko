5ba9619c1059cb0d2e39af861306420867ad2964
ostre8585 <ostre8585@f4b0abea-410c-0df8-8af4-832eedf32b06>
2011-05-28 09:13:08 +0000

    결과 커밋 UI대강

---
+++ pl/src/com/poolc/pl/TotalOption.java
@@ -0,0 +1,16 @@
+package com.poolc.pl;
+
+
+import android.app.Activity;
+import android.app.TabActivity;
+import android.os.Bundle;
+
+public class TotalOption extends Activity{
+
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.total_option);
+		
+	}
+}
