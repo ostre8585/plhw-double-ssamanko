a9ba71436fb696beb2f1ddf390d0f7eaa0750f8c
yoite@lycos.co.kr <yoite@lycos.co.kr@f4b0abea-410c-0df8-8af4-832eedf32b06>
2011-05-29 11:31:22 +0000

    network client

---
+++ pl/src/com/poolc/pl/network/MobileClient.java
@@ -0,0 +1,52 @@
+package com.poolc.pl.network;
+
+
+import java.io.*;
+import java.net.*;
+
+public class MobileClient {
+	private final static MobileClient instance = new MobileClient();
+	private BufferedReader br;
+	private PrintWriter pw;
+	
+	private Socket sock;
+	
+	public MobileClient() {
+		try {
+		
+			sock = new Socket("127.0.0.1", 10002);
+			br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			pw = new PrintWriter(new OutputStreamWriter(sock.getOutputStream()));
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static MobileClient getInstance() {
+		return instance;
+	}
+	
+	public void run() {
+		//do something
+		
+		
+		
+		
+		
+		//when sock close 
+//		try {
+//			br.close();
+//			pw.close();
+//			sock.close();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+	}
+	
+}
