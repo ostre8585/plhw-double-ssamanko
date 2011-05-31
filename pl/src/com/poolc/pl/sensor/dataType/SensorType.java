f68c800e9312482b395a33779f2b36fef3d8c759
yoite@lycos.co.kr <yoite@lycos.co.kr@f4b0abea-410c-0df8-8af4-832eedf32b06>
2011-05-31 17:05:59 +0000

    ManageSensor.java, SensorType.java

--- pl/src/com/poolc/pl/sensor/dataType/SensorType.java
+++ pl/src/com/poolc/pl/sensor/dataType/SensorType.java
@@ -3,5 +3,11 @@ package com.poolc.pl.sensor.dataType;
 //센서 종류를 상수화 시켜서 사용하기 위해 규정된 클래스임.
 public class SensorType {
 	public static int ACCELEROMETERSENSOR = 0;
-	public static int LIGHTSENSOR = 1; 
+	public static int BATTERY = 1;
+	public static int GPSSENSOR = 2;
+	public static int GYROSCOPESENSOR = 3;
+	public static int LIGHTSENSOR = 4;
+	public static int MAGNETICFIELDSENSOR = 5;
+	public static int ORIENTATIONSENSOR = 6;
+	public static int PROXIMITYSENSOR = 7;
 }
