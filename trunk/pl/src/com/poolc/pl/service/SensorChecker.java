7a73313bd98f09c7994def936198fc18c013ed15
yoite@lycos.co.kr <yoite@lycos.co.kr@f4b0abea-410c-0df8-8af4-832eedf32b06>
2011-05-31 16:43:10 +0000

    SensorChecker.java

--- trunk/pl/src/com/poolc/pl/service/SensorChecker.java
+++ trunk/pl/src/com/poolc/pl/service/SensorChecker.java
@@ -1,16 +1,25 @@
 package com.poolc.pl.service;
 
 import com.poolc.pl.sensor.dataType.DataCommandDto;
-import com.poolc.pl.sensor.sensorControler.AccelerometerSensorControler;
+import com.poolc.pl.sensor.sensorControler.*;
+
 
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.hardware.SensorManager;
+import android.location.LocationManager;
 import android.os.IBinder;
 
 public class SensorChecker extends Service{
 	private AccelerometerSensorControler accelerometerSensorControler = null;
+	private BatteryControler batteryControler = null;
+	private GpsSensorControler gpsSensorControler = null;
+	private GyroscopeSensorControler gyroscopeSensorControler = null;
+	private LightSensorControler lightSensorControler = null;
+	private MagneticSensorControler magneticSensorControler = null;
+	private OrientationSensorControler orientationSensorControler = null;
+	private ProximitySensorControler proximitySensorControler = null;
 
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -21,6 +30,14 @@ public class SensorChecker extends Service{
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId){
 		startAccelerometerSensorCrawler(DataCommandDto.accelerometerDelayTime);
+		startBatteryCrawler(DataCommandDto.batteryDelayTime);
+		startGpsSensorCrawler(DataCommandDto.gpsDelayTime);
+		startGyroscopeSensorCrawler(DataCommandDto.gyroscopeDelayTime);
+		startLightSensorCrawler(DataCommandDto.lightDelayTime);
+		startMagneticSensorCrawler(DataCommandDto.magneticFieldDelayTime);
+		startOrientationSensorCrawler(DataCommandDto.orientationDelayTime);
+		startProximitySensorCrawler(DataCommandDto.proximityDelayTime);
+		
 		return super.onStartCommand(intent, flags, startId);
 	}
 	
@@ -28,6 +45,13 @@ public class SensorChecker extends Service{
 	public void onDestroy(){
 		
 		endAccelerometerSensorCrawler();
+		endBatteryCrawler();
+		endGpsSensorCrawler();
+		endGyroscopeSensorCrawler();
+		endLightSensorCrawler();
+		endMagneticSensorCrawler();
+		endOrientationSensorCrawler();
+		endProximitySensorCrawler();
 	}
 	
 	private void startAccelerometerSensorCrawler(double accelerometerDelayTime){
@@ -42,4 +66,93 @@ public class SensorChecker extends Service{
 			return;
 		accelerometerSensorControler.interrupt();
 	}
+	private void startBatteryCrawler(double batteryDelayTime) {
+		if(batteryControler == null) {
+			batteryControler = new BatteryControler(this, (long)(batteryDelayTime*1000));
+		}
+		batteryControler.start();
+	}
+	
+	private void endBatteryCrawler() {
+		if(batteryControler == null && !batteryControler.isAlive())
+			return;
+		batteryControler.interrupt();
+	}
+	private void startGpsSensorCrawler(double gpsDelayTime) {
+		if(gpsSensorControler == null) {
+			gpsSensorControler = new GpsSensorControler(this, (LocationManager)getSystemService(Context.LOCATION_SERVICE), (long) (gpsDelayTime*1000));
+		}
+		gpsSensorControler.start();
+	}
+	private void endGpsSensorCrawler() {
+		if(gpsSensorControler == null && !gpsSensorControler.isAlive())
+			return;
+		gpsSensorControler.interrupt();
+	}
+	private void startGyroscopeSensorCrawler(double gyroscopeDelayTime) {
+		if(gyroscopeSensorControler == null) {
+			gyroscopeSensorControler = new GyroscopeSensorControler(this, (SensorManager)getSystemService(Context.SENSOR_SERVICE), (long)(gyroscopeDelayTime*1000));
+		}
+		gyroscopeSensorControler.start();
+	}
+	
+	private void endGyroscopeSensorCrawler() {
+		if(gyroscopeSensorControler == null && !gyroscopeSensorControler.isAlive())
+			return;
+		gyroscopeSensorControler.interrupt();
+		
+	}
+	
+	private void startLightSensorCrawler(double lightDelayTime) {
+		if(lightSensorControler == null) {
+			lightSensorControler = new LightSensorControler(this, (SensorManager)getSystemService(Context.SENSOR_SERVICE), (long)(lightDelayTime*1000));
+		}
+		lightSensorControler.start();
+	}
+	
+	private void endLightSensorCrawler() {
+		if(lightSensorControler == null && !lightSensorControler.isAlive())
+			return;
+		lightSensorControler.interrupt();
+	}
+	
+	private void startMagneticSensorCrawler(double magneticDelayTime) {
+		if(magneticSensorControler == null) {
+			magneticSensorControler = new MagneticSensorControler(this, (SensorManager)getSystemService(Context.SENSOR_SERVICE), (long)(magneticDelayTime*1000));
+		}
+		magneticSensorControler.start();
+	}
+	
+	private void endMagneticSensorCrawler() {
+		if(magneticSensorControler == null && !magneticSensorControler.isAlive())
+			return;
+		magneticSensorControler.interrupt();
+	}
+	
+	private void startOrientationSensorCrawler(double orientationDelayTime) {
+		if(orientationSensorControler == null) {
+			orientationSensorControler = new OrientationSensorControler(this, (SensorManager)getSystemService(Context.SENSOR_SERVICE), (long)(orientationDelayTime*1000));
+		}
+		orientationSensorControler.start();
+	}
+	
+	private void endOrientationSensorCrawler() {
+		if(orientationSensorControler == null && !orientationSensorControler.isAlive())
+			return;
+		orientationSensorControler.interrupt();
+	}
+	
+	private void startProximitySensorCrawler(double proximityDelayTime) {
+		if(proximitySensorControler == null) {
+			proximitySensorControler = new ProximitySensorControler(this, (SensorManager)getSystemService(Context.SENSOR_SERVICE), (long)(proximityDelayTime*1000));
+		}
+		proximitySensorControler.start();
+	}
+	
+	private void endProximitySensorCrawler() {
+		if(proximitySensorControler == null && !proximitySensorControler.isAlive())
+			return;
+		proximitySensorControler.interrupt();
+	}
+	
 }
